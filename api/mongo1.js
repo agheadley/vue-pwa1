0b5cbfb29a85994c86405b917a2192ef7c8b90eb
agheadley <agheadley@gmail.com>
2020-08-14 13:07:49 +0100

    added vuetify/mongo/env/brcrypt3

--- api/mongo1.js
+++ api/mongo1.js
@@ -47,5 +47,5 @@ module.exports = async (req, res) => {
 
   // Respond with a JSON string of all users in the collection
   res.status(200).json({ users });
-  res.status(200).json(query);
+  //res.status(200).json(query);
 };
